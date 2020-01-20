3251cde8d28c472ce4ff77df28c9b3d7ca401bd2
Chris <kszczyrbak8@gmail.com>
2020-01-20 16:30:54 +0100

    added influx scripts for anomaly detection

---
+++ scripts/influx/detect_anomaly.py
@@ -0,0 +1,50 @@
+from influxdb import InfluxDBClient
+
+### InfluxDB info ####
+influx_db_name = "sensors"
+influxClient = InfluxDBClient("localhost", "8086", database=influx_db_name)
+
+result = influxClient.query(
+    """
+    SHOW TAG VALUES ON "sensors" WITH KEY = "detector_id" 
+    """
+)
+
+detectors = [{dict['key'] : dict['value']} for dict in list(result.get_points())]
+
+thresholds = influxClient.query(
+        """
+        SELECT
+        percentile_25 - 1.5*iqr as lower_threshold,
+        percentile_75 + 1.5*iqr as upper_threshold
+        FROM
+        (
+            SELECT
+                percentile(traffic_diff, 25) as percentile_25,
+                percentile(traffic_diff, 75) as percentile_75,
+                percentile(traffic_diff, 75) - percentile(traffic_diff, 25) as iqr
+            FROM 
+            (
+                SELECT non_negative_difference(count) AS traffic_diff from "sensors".."traffic"
+            )
+        ) group by detector_id 
+        """
+    )
+
+dataQuery = (
+    """
+    SELECT * 
+    INTO "sensors".."anomalies"
+    FROM 
+    (
+        SELECT non_negative_difference(count) from "sensors".."traffic" group by *
+    )
+    WHERE diff < {} or diff > {} group by *
+    """
+)
+
+for detector in detectors[:100]:
+    result = list(thresholds.get_points(tags=detector))
+    if len(result) > 0:
+        threshold_dict = result[0]
+        influxClient.query(dataQuery.format(threshold_dict['lower_threshold'], threshold_dict['upper_threshold']))
---
+++ scripts/influx/migrate.py
@@ -0,0 +1,71 @@
+### MySQL DB info ###
+#import MySQLdb
+# conn = MySQLdb.connect(host="localhost",  # your host, usually localhost
+#                     user="john",         # your username
+#                     passwd="megajonhy",  # your password
+#                     db="jonhydb")        # name of the data base
+
+
+### PostgreSQL DB info ###
+from influxdb import InfluxDBClient
+import psycopg2
+import psycopg2.extras
+postgresql_table_name = ""
+conn = psycopg2.connect("dbname=postgres user=postgres " +
+                        "password=postgres host=localhost")
+
+### InfluxDB info ####
+influx_db_name = "sensors"
+influxClient = InfluxDBClient("localhost", "8086", database=influx_db_name)
+influxClient.drop_database(influx_db_name)
+influxClient.create_database(influx_db_name)
+
+# dictates how columns will be mapped to key/fields in InfluxDB
+schema = {
+    "time_column": "starttime",  # the column that will be used as the time stamp in influx
+    "columns_to_fields": ["count"],  # columns that will map to fields
+    "columns_to_tags": ["detector_id"],  # columns that will map to tags
+    "table_name_to_measurement": "traffic",  # table name that will be mapped to measurement
+}
+
+'''
+Generates an collection of influxdb points from the given SQL records
+'''
+
+
+def generate_influx_points(records):
+    influx_points = []
+    for record in records:
+        tags = {}
+        fields = {}
+
+        for tag_label in schema['columns_to_tags']:
+            tags[tag_label] = record[tag_label]
+        for field_label in schema['columns_to_fields']:
+            fields[field_label] = record[field_label]
+        influx_points.append({
+            "measurement": schema['table_name_to_measurement'],
+            "tags": tags,
+            "time": record[schema['time_column']],
+            "fields": fields
+        })
+    return influx_points
+
+
+# query relational DB for all records
+curr = conn.cursor('cursor', cursor_factory=psycopg2.extras.RealDictCursor)
+# curr = conn.cursor(dictionary=True)
+curr.execute("SELECT * FROM " + schema['table_name_to_measurement'] +
+             " ORDER BY " + schema['time_column'] + " DESC limit 200000;")
+row_count = 0
+# process 1000 records at a time
+while True:
+    print("Processing row {}".format(row_count + 1))
+    selected_rows = curr.fetchmany(1000)
+    print("Write points")
+    influxClient.write_points(generate_influx_points(selected_rows))
+    print("Points written")
+    row_count += 1000
+    if len(selected_rows) < 1000:
+        break
+conn.close()
